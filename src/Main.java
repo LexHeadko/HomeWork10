b87435c093316cd708f443ed04458a5f8ab2b9d2
LexHeadko <n-odes11rus@yandex.ru>
2023-01-30 19:11:02 +0200

    Initial commit!

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        //task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        //task8();
+        //task9();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+    }
+}
