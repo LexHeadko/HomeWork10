fb5cb9f56f6f1d762f587a5b3024495f84ead424
LexHeadko <n-odes11rus@yandex.ru>
2023-01-30 21:51:04 +0200

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        //task3();
+        task3();
         //task4();
         //task5();
         //task6();
@@ -27,5 +27,14 @@ public class Main {
         String lastName = "Ivanovich";
         String fullName = firstName + ' ' + middleName + ' ' + lastName;
         System.out.println(fullName.toUpperCase());
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        String firstName = "Иванов";
+        String middleName = "Семён";
+        String lastName = "Семёнович";
+        String fullName = firstName + ' ' + middleName + ' ' + lastName;
+        fullName = fullName.replace('ё', 'е');
+        System.out.println(fullName);
     }
 }
